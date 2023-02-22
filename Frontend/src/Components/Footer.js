42099cdc62318e511ecc2dbca26307a2da38d242
Pradyumancoder <shuklapradyuman786@gmail.com>
2023-02-22 14:51:40 +0530

    almostdone

--- Frontend/src/Components/Footer.js
+++ Frontend/src/Components/Footer.js
@@ -6,17 +6,17 @@ import { IoMdMail } from 'react-icons/io'
 export const Footer = () => {
   return (
     <div className='bg-gray-500 text-center py-3 text-white'>
-        <h3 className='font-medium'>Made with ❤️ by Hemant Prajapati</h3>
+        <h3 className='font-medium'>Made with ❤️ by Pradyuman Shukla</h3>
         <div>
         <p>Connect me</p>
             <div className='flex justify-center gap-3'>
-                <a className='hover:scale-110' href="https://www.linkedin.com/in/hemant-prajapatii/">
+                <a className='hover:scale-110' href="https://www.linkedin.com/in/pradyuman-shukla/">
                 <ImLinkedin />
                 </a>
-                <a className='hover:scale-110' href="https://github.com/hemant069">
+                <a className='hover:scale-110' href="https://github.com/Pradyumancoder">
                 <BsGithub />
                 </a>
-                <a className='hover:scale-110' href="mailto:hemantprajapati20500@gmail.com">
+                <a className='hover:scale-110' href="mailto:shuklapradyuman786@gmail.com">
                 <IoMdMail />
                 </a>
             </div>

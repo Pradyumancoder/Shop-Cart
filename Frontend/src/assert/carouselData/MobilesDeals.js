c93dbc0dee3c9e4cee0813b0872af1e491866d49
Pradyumancoder <shuklapradyuman786@gmail.com>
2023-02-22 13:41:34 +0530

    carousel

---
+++ Frontend/src/assert/carouselData/MobilesDeals.js
@@ -0,0 +1,9 @@
+
+
+export const MobilesDeals=[
+   'https://www.jiomart.com/images/cms/aw_rbslider/slides/1669784486_Mobile-05.jpg', 
+  'https://www.jiomart.com/images/cms/aw_rbslider/slides/1669784519_Mobile-01.jpg',
+  'https://www.jiomart.com/images/cms/aw_rbslider/slides/1669784534_Mobile-02.jpg',
+  'https://www.jiomart.com/images/cms/aw_rbslider/slides/1669784506_Mobile-03.jpg',
+
+]
---
+++ Frontend/src/assert/carouselData/TopDeal.js
@@ -0,0 +1,9 @@
+
+
+export const TopDeal=[
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1674756979_MonthEndDeals_1680x320.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1674755921_Redmi_Smartphones_Desktop.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1674756604_Laptop-Carnival-JioMart--Carousel-Banner-13_09_2022_1680x320.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1675107852_1675014323_1674841079_1674797556_1674794009_HPMC_-1_1680x320_copy_32.jpg',
+
+]
